57e5cf538010e82bec250a32ecf7f7d437c38427
marline <marlinekhavele6@gmail.com>
2017-08-17 18:58:52 +0300

    my first commit

---
+++ app/src/main/java/com/example/student/myfirstapp/SmsBroadcastReceiver.java
@@ -0,0 +1,10 @@
+package com.example.student.myfirstapp;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by Student on 6/13/2017.
+ */
+
+public abstract class SmsBroadcastReceiver extends BroadcastReceiver {
+}
